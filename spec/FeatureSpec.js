b32d58acb7fa23ba520637b9e8ab50153790b392
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-03 12:00:41 +0100

    Planes are cleared for takeoff - completed - tests passing

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -12,6 +12,10 @@ describe('Feature Test', function(){
   it('planes can be instructed to land at an airport', function() {
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
-  } );
-
+  });
+  it('planes can be instructed to takeoff', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
 });
